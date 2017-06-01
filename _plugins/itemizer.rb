a20858038e36c31894d0e8e386e75ffc2839c971
Milo Watanabe <milo.wata@gmail.com>
2017-06-01 09:40:30 -1000

    write some comments

--- _plugins/itemizer.rb
+++ _plugins/itemizer.rb
@@ -1,6 +1,7 @@
 # Jekyll plugin to itemize each post for JSON output
 # Parses the current pattern of every item beginning with
 # <p><Number>/ ...
+
 require 'jekyll'
 require 'json'
 
@@ -10,9 +11,14 @@ module Jekyll
 
     def itemize(post)
 
-
+      # splits the contents into items for each story. Slice off
+      # the first one, since it's an empty string
       item_list = post.content.split(/<p>\d\//).slice(1..-1)
+
+      # make the list into an indexed array
       items = Hash[(1...item_list.size).zip item_list]
+
+      # setup the entire output with any other desired keys
       output = ({
         :title        => post['title'],
         :description  => post['description'],
@@ -20,6 +26,7 @@ module Jekyll
         :items        => items
       })
 
+      # ship it!
       output.to_json
     end
 

67ccc238e58e08adaebebfe1e5a03fb672c3047b
mkiser <2197111+mkiser@users.noreply.github.com>
2018-02-09 15:31:12 -0800

    quiz

--- public/js/quiz/slickQuiz-config.js
+++ public/js/quiz/slickQuiz-config.js
@@ -15,60 +15,59 @@ var quizJSON = {
     },
     "questions": [
         { // Question 1 - Multiple Choice, Single True Answer
-            "q": "Who told Trump that the emails involving Trump Jr. and the Trump Tower meeting \"will never get out\"?",
+            "q": "Who did Trump accuse of being un-American and treasonous?",
             "a": [
-                {"option": "Sean Spicer",      "correct": false},
-                {"option": "Sarah Huckabee Sanders",     "correct": false},
-                {"option": "Hope Hicks",      "correct": true},
-                {"option": "Devin Nunes",     "correct": false} // no comma here
+                {"option": "Adam Schiff",      "correct": false},
+                {"option": "Rob Porter",     "correct": false},
+                {"option": "Democrats",      "correct": true},
+                {"option": "Nancy Pelosi",     "correct": false} // no comma here
             ],
-            "correct": "<p><strong>I HOPE you're correct.</strong> Hope Hicks allegedly told Trump that the emails involving Trump Jr. and the Trump Tower meeting \"will never get out\" because only a few people have access to them. (<a href='https://whatthefuckjusthappenedtoday.com/2018/02/01/day-378/' target='_blank'>Day 378</a> / <a href='https://www.nytimes.com/2018/02/01/us/politics/republicans-secret-memo-nunes.html' target='_blank'>New York Times</a>)</p>",
-            "incorrect": "<p><strong>I HOPE you pay attention next week.</strong> Hope Hicks allegedly told Trump that the emails involving Trump Jr. and the Trump Tower meeting 'will never get out' because only a few people have access to them. (<a href='https://whatthefuckjusthappenedtoday.com/2018/02/01/day-378/' target='_blank'>Day 378</a> / <a href='https://www.nytimes.com/2018/02/01/us/politics/republicans-secret-memo-nunes.html' target='_blank'>New York Times</a>)</p>" // no comma here
+            "correct": "<p><strong>Those damn Dems.</strong> Trump accused Democrats of being un-American and treasonous because they didn't clap for him during his State of the Union address. \"Can we call that treason?\" Trump asked during a speech at a factory in Ohio. \"Why not? I mean, they certainly didn't seem to love our country very much.\" He added: \"Your paychecks are going way up\" and \"your taxes are going way down.\"  (<a href='https://whatthefuckjusthappenedtoday.com/2018/02/05/day-382/#1-trump-accused-democrats-of-being-u' target='_blank'>Day 382</a> / <a href='https://www.nytimes.com/2018/02/05/us/politics/trump-accuses-democrats-treason-market-rout.html' target='_blank'>New York Times</a>)</p>",
+            "incorrect": "<p><strong>Damn the Dems.</strong> Trump accused Democrats of being un-American and treasonous because they didn't clap for him during his State of the Union address. \"Can we call that treason?\" Trump asked during a speech at a factory in Ohio. \"Why not? I mean, they certainly didn't seem to love our country very much.\" He added: \"Your paychecks are going way up\" and \"your taxes are going way down.\"  (<a href='https://whatthefuckjusthappenedtoday.com/2018/02/05/day-382/#1-trump-accused-democrats-of-being-u' target='_blank'>Day 382</a> / <a href='https://www.nytimes.com/2018/02/05/us/politics/trump-accuses-democrats-treason-market-rout.html' target='_blank'>New York Times</a>)</p>" // no comma here
         },
         { // Question 2 - Multiple Choice, Multiple True Answers, Select Any
-            "q": "Trump asked __________ if he was 'on my team?'",
+            "q": "Who said there is \"clear evidence that the Russians meddled\" in the 2016 presidential election?",
             "a": [
                 {"option": "James Comey", "correct": false},
-                {"option": "Rod Rosenstein",   "correct": true},
-                {"option": "Jeff Sessions",  "correct": false},
-                {"option": "Steve Bannon", "correct": false} // no comma here
+                {"option": "Robert Mueller",   "correct": false},
+                {"option": "Joe Biden",  "correct": false},
+                {"option": "George W. Bush", "correct": true} // no comma here
             ],
-            "correct": "<p><strong>YUP.</strong> Trump asked Rod Rosenstein if he was \"on my team\" during a December meeting at the White House. \"Of course, we're all on your team, Mr. President,\" Rosenstein said, who wanted Trump to push back on the Nunes memo. Trump, however, wanted to know where Robert Mueller's Russia investigation was going. It's Trump's fourth loyalty request from a Justice Department official. (<a href='https://whatthefuckjusthappenedtoday.com/2018/01/31/day-377/' target='_blank'>Day 377</a> / <a href='https://www.cnn.com/2018/01/31/politics/donald-trump-rod-rosenstein-december-meeting/index.html' target='_blank'>CNN</a>)</p>",
-            "incorrect": "<p><strong>NOPE.</strong> Trump asked Rod Rosenstein if he was \"on my team\" during a December meeting at the White House. \"Of course, we're all on your team, Mr. President,\" Rosenstein said, who wanted Trump to push back on the Nunes memo. Trump, however, wanted to know where Robert Mueller's Russia investigation was going. It's Trump's fourth loyalty request from a Justice Department official. (<a href='https://whatthefuckjusthappenedtoday.com/2018/01/31/day-377/' target='_blank'>Day 377</a> / <a href='https://www.cnn.com/2018/01/31/politics/donald-trump-rod-rosenstein-december-meeting/index.html' target='_blank'>CNN</a>)</p>" // no comma here
+            "correct": "<p><strong>W stands for you won.</strong> George W. Bush: \"Clear evidence that the Russians meddled\" in the 2016 presidential election. Intelligence agencies have concluded that Russia meddled in the presidential election, but Trump has consistently disputed allegations that members of his campaign team in any way \"colluded\" with Moscow.  (<a href='https://whatthefuckjusthappenedtoday.com/2018/02/08/day-385/#george-w-bush-clear-evidence-that-th' target='_blank'>Day 385</a> / <a href='https://www.usatoday.com/story/news/2018/02/08/george-w-bush-clear-evidence-russians-meddled-election/318620002/' target='_blank'>USA Today</a>)</p>",
+            "incorrect": "<p><strong>Sorry, but you're beating around the Bush.</strong> George W. Bush: \"Clear evidence that the Russians meddled\" in the 2016 presidential election. Intelligence agencies have concluded that Russia meddled in the presidential election, but Trump has consistently disputed allegations that members of his campaign team in any way \"colluded\" with Moscow.  (<a href='https://whatthefuckjusthappenedtoday.com/2018/02/08/day-385/#george-w-bush-clear-evidence-that-th' target='_blank'>Day 385</a> / <a href='https://www.usatoday.com/story/news/2018/02/08/george-w-bush-clear-evidence-russians-meddled-election/318620002/' target='_blank'>USA Today</a>)</p>" // no comma here
         },
         { // Question 3 - Multiple Choice, Multiple True Answers, Select All
-            "q": "Sarah Huckabee Sanders claimed that \"poll after poll\" says that nobody cares about ____________?",
+            "q": "What percent of Americans believe Russia will try to influence this year's midterm elections?",
             "a": [
-                {"option": "Trump's poll numbers",           "correct": false},
-                {"option": "The Patriots being in the Super Bowl again",                  "correct": false},
-                {"option": "Turtles",  "correct": false},
-                {"option": "The Trump-Russia investigation",          "correct": true} // no comma here
+                {"option": "54%",           "correct": false},
+                {"option": "55%",                  "correct": false},
+                {"option": "56%",  "correct": false},
+                {"option": "57%",          "correct": true} // no comma here
             ],
-            "correct": "<p><strong>Totally correct.</strong> Sarah Huckabee Sanders claimed that \"poll after poll\" says that nobody cares about the Trump-Russia investigation. She did not cite a specific poll. However, poll, after poll, after poll, after poll, after poll, after poll, after poll suggest Americans do care about the issue. (<a href='https://whatthefuckjusthappenedtoday.com/2018/01/29/day-375/' target='_blank'>Day 375</a> / <a href='https://www.washingtonpost.com/news/the-fix/wp/2018/01/29/polls-show-no-one-cares-about-the-russia-investigation-white-house-press-secretary-said-thats-not-true/' target='_blank'>Washington Post</a>)</p>",
-            "incorrect": "<p><strong>Really?</strong> Sarah Huckabee Sanders claimed that \"poll after poll\" says that nobody cares about the Trump-Russia investigation. She did not cite a specific poll. However, poll, after poll, after poll, after poll, after poll, after poll, after poll suggest Americans do care about the issue. (<a href='https://whatthefuckjusthappenedtoday.com/2018/01/29/day-375/' target='_blank'>Day 375</a> / <a href='https://www.washingtonpost.com/news/the-fix/wp/2018/01/29/polls-show-no-one-cares-about-the-russia-investigation-white-house-press-secretary-said-thats-not-true/' target='_blank'>Washington Post</a>)</p>" // no comma here
+            "correct": "<p><strong>100% correct.</strong> 57% of Americans believe Russia will try to influence this year's midterm elections and 55% believe the federal government isn't doing enough to prevent it.  (<a href='https://whatthefuckjusthappenedtoday.com/2018/02/08/day-385/#poll-57-of-americans-believe-russia' target='_blank'>Day 358</a> / <a href='https://www.nbcnews.com/politics/politics-news/poll-most-americans-think-russia-will-interfere-again-2018-elections-n845076' target='_blank'>NBC News</a>)</p>",
+            "incorrect": "<p><strong>0% correct</strong> 57% of Americans believe Russia will try to influence this year's midterm elections and 55% believe the federal government isn't doing enough to prevent it.  (<a href='https://whatthefuckjusthappenedtoday.com/2018/02/08/day-385/#poll-57-of-americans-believe-russia' target='_blank'>Day 358</a> / <a href='https://www.nbcnews.com/politics/politics-news/poll-most-americans-think-russia-will-interfere-again-2018-elections-n845076' target='_blank'>NBC News</a>)</p>" // no comma here
         },
         { // Question 4
-            "q": "Trump will not impose new sanctions on Russia because ____________.",
+            "q": "Paul Ryan celebrated a secretary's weekly wag increase on Twitter as a sign of the Republican tax plan's success. How much did her weekly paycheck go up?",
             "a": [
-                {"option": "The threat is already \"serving as a deterrent\"",    "correct": true},
-                {"option": "It would be \"extraordinarily reckless\"",     "correct": false},
-                {"option": "The FBI had \"grave concerns\"",      "correct": false},
-                {"option": "It would be \"treasonous\"",   "correct": false} // no comma here
+                {"option": "$0.75",     "correct": false},
+                {"option": "$1.50",    "correct": true},
+                {"option": "$4.50",      "correct": false},
+                {"option": "$17.50",   "correct": false} // no comma here
             ],
-            "select_any": true,
-            "correct": "<p><strong>Apparently so.</strong> Trump will not impose new sanctions on Russia because the threat is already \"serving as a deterrent,\" a State Department official said. A bipartisan bill overwhelmingly passed in July imposes penalties on companies doing \"significant\" business with Russian defense and intelligence entities. State Department spokeswoman Heather Nauert said: \"We estimate that foreign governments have abandoned planned or announced purchases of several billion dollars in Russian defense acquisitions.\"  (<a href='https://whatthefuckjusthappenedtoday.com/2018/01/30/day-376/' target='_blank'>Day 376</a> / <a href='https://www.politico.com/story/2018/01/29/russia-sanctions-white-house-congress-376813' target='_blank'>Politico</a>)</p>",
-            "incorrect": "<p><strong>Fake news.</strong> Trump will not impose new sanctions on Russia because the threat is already \"serving as a deterrent,\" a State Department official said. A bipartisan bill overwhelmingly passed in July imposes penalties on companies doing \"significant\" business with Russian defense and intelligence entities. State Department spokeswoman Heather Nauert said: \"We estimate that foreign governments have abandoned planned or announced purchases of several billion dollars in Russian defense acquisitions.\"  (<a href='https://whatthefuckjusthappenedtoday.com/2018/01/30/day-376/' target='_blank'>Day 376</a> / <a href='https://www.politico.com/story/2018/01/29/russia-sanctions-white-house-congress-376813' target='_blank'>Politico</a>)</p>" // no comma here
+            "correct": "<p><strong>Cha-ching!</strong> Paul Ryan celebrated a secretary's $1.50 weekly increase on Twitter as a sign of the Republican tax plan's success. He deleted the tweet after lawmakers and social media users criticized him. \"A secretary at a public high school in Lancaster, Pennsylvania, said she was pleasantly surprised her pay went up $1.50 a week … she said [that] will more than cover her Costco membership for the year,\" the tweet read. (<a href='https://whatthefuckjusthappenedtoday.com/2018/02/05/day-382/#6-paul-ryan-celebrated-a-secretarys' target='_blank'>Day 382</a> / <a href='https://www.nytimes.com/2018/02/03/us/politics/paul-ryan-tweet.html' target='_blank'>New York Times</a>)</p>",
+            "incorrect": "<p><strong>Fake news.</strong> Paul Ryan celebrated a secretary's $1.50 weekly increase on Twitter as a sign of the Republican tax plan's success. He deleted the tweet after lawmakers and social media users criticized him. \"A secretary at a public high school in Lancaster, Pennsylvania, said she was pleasantly surprised her pay went up $1.50 a week … she said [that] will more than cover her Costco membership for the year,\" the tweet read. (<a href='https://whatthefuckjusthappenedtoday.com/2018/02/05/day-382/#6-paul-ryan-celebrated-a-secretarys' target='_blank'>Day 382</a> / <a href='https://www.nytimes.com/2018/02/03/us/politics/paul-ryan-tweet.html' target='_blank'>New York Times</a>)</p>" // no comma here
         },
         { // Question 5
-            "q": "What was Trump's average 2017 job approval rating?",
+            "q": "Who said the White House is in \"total cooperation mode?\"",
             "a": [
-                {"option": "34%",    "correct": false},
-                {"option": "36%",     "correct": false},
-                {"option": "38%",      "correct": true},
-                {"option": "40%",   "correct": false} // no comma here
+                {"option": "Steve Bannon",    "correct": false},
+                {"option": "Sarah Huckabee Sanders",     "correct": false},
+                {"option": "Ty Cobb",      "correct": true},
+                {"option": "Jeff Sessions",   "correct": false} // no comma here
             ],
-            "correct": "<p><strong>You must like math – CORRECT.</strong> Trump's 2017 job approval rating averaged 38% throughout the U.S., ranging from a high of 61% in West Virginia to a low of 26% in Vermont. Trump averaged 50% or higher approval in 12 states in total. By comparison, Obama had an approval rate of 50% or greater in 41 states in his first year in office. (<a href='https://whatthefuckjusthappenedtoday.com/2018/01/30/day-376/' target='_blank'>Day 366</a> / <a href='http://news.gallup.com/poll/226454/trump-approval-highest-west-virginia-lowest-vermont.aspx' target='_blank'>Gallup</a>)</p>",
-            "incorrect": "<p><strong>That was a good guess, but no.</strong>Trump's 2017 job approval rating averaged 38% throughout the U.S., ranging from a high of 61% in West Virginia to a low of 26% in Vermont. Trump averaged 50% or higher approval in 12 states in total. By comparison, Obama had an approval rate of 50% or greater in 41 states in his first year in office. (<a href='https://whatthefuckjusthappenedtoday.com/2018/01/30/day-376/' target='_blank'>Day 366</a> / <a href='http://news.gallup.com/poll/226454/trump-approval-highest-west-virginia-lowest-vermont.aspx' target='_blank'>Gallup</a>)</p>" // no comma here
+            "correct": "<p><strong>Yep.</strong> Trump's lawyers want him to refuse an interview with Robert Mueller, because they're concerned that he could be charged with lying to investigators. Trump, however, has said that he is \"looking forward\" to speaking with Mueller as part of the investigation into possible collusion between his campaign and Russia's election interference, and whether he obstructed justice. Ty Cobb, the White House lawyer Trump tapped to deal with Mueller's investigation, has said the White House is in \"total cooperation mode.\" (<a href='https://whatthefuckjusthappenedtoday.com/2018/02/06/day-383/#1-trumps-lawyers-want-him-to-refuse' target='_blank'>Day 383</a> / <a href='https://www.nytimes.com/2018/02/05/us/politics/trump-lawyers-special-counsel-interview.html' target='_blank'>New York Times</a>)</p>",
+            "incorrect": "<p><strong>I appreciate your effort. You're wrong.</strong> Trump's lawyers want him to refuse an interview with Robert Mueller, because they're concerned that he could be charged with lying to investigators. Trump, however, has said that he is \"looking forward\" to speaking with Mueller as part of the investigation into possible collusion between his campaign and Russia's election interference, and whether he obstructed justice. Ty Cobb, the White House lawyer Trump tapped to deal with Mueller's investigation, has said the White House is in \"total cooperation mode.\" (<a href='https://whatthefuckjusthappenedtoday.com/2018/02/06/day-383/#1-trumps-lawyers-want-him-to-refuse' target='_blank'>Day 383</a> / <a href='https://www.nytimes.com/2018/02/05/us/politics/trump-lawyers-special-counsel-interview.html' target='_blank'>New York Times</a>)</p>" // no comma here
         } // no comma here
     ]
 };

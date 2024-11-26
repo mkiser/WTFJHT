4f296a24fa1fde4243c9b95b562bf8334d7a85ad
Matt <matt@whatthefuckjusthappenedtoday.com>
2024-11-26 09:23:41 -0800

    more updates

--- _plugins/years_ago_links.rb
+++ _plugins/years_ago_links.rb
@@ -16,11 +16,16 @@ module Jekyll
 
       # Iterate for 1 to 6 years ago
       (1..6).each do |years_ago|
-        target_date = (current_date - (years_ago * 365)).strftime('%Y-%m-%d')
+        target_year = current_date.year - years_ago
+        target_month = current_date.month
+        target_day = current_date.day
 
-        # Find a post from the same date in previous years, excluding the current post
+        # Find a post with the same month and day in the target year
         matching_post = site.posts.docs.find do |p|
-          p.data['date'].strftime('%Y-%m-%d') == target_date && p != current_post
+          p.data['date'].year == target_year &&
+          p.data['date'].month == target_month &&
+          p.data['date'].day == target_day &&
+          p != current_post
         end
 
         if matching_post

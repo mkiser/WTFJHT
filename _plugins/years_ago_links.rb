3eb15f5edfa646410c621849781aaf846bd2a110
Matt <matt@whatthefuckjusthappenedtoday.com>
2024-11-26 08:54:00 -0800

    update plugin

--- _plugins/years_ago_links.rb
+++ _plugins/years_ago_links.rb
@@ -12,14 +12,16 @@ module Jekyll
 
     def generate_years_ago_links(site, current_post)
       current_date = current_post.data['date']
-      years_ago_data = {}
+      years_ago_data = []
 
+      # Iterate for 1 to 6 years ago
       (1..6).each do |years_ago|
         target_date = (current_date - (years_ago * 365)).strftime('%Y-%m-%d')
         matching_post = site.posts.docs.find { |p| p.data['date'].strftime('%Y-%m-%d') == target_date }
 
         if matching_post
-          years_ago_data[years_ago] = {
+          years_ago_data << {
+            'years_ago' => years_ago,
             'title' => matching_post.data['title'],
             'desc' => matching_post.data['description'] || '',
             'url' => matching_post.url

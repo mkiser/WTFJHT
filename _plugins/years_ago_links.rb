157844c30333afcceb5b7ecd2a6bf994d13b4e12
Matt Kiser <2197111+mkiser@users.noreply.github.com>
2025-01-02 08:44:05 -0800

    Update years_ago_links.rb

--- _plugins/years_ago_links.rb
+++ _plugins/years_ago_links.rb
@@ -12,14 +12,22 @@ module Jekyll
 
     def generate_years_ago_links(site, current_post)
       current_date = current_post.data['date']
+      blog_start_year = 2017
+      current_year = Date.today.year
+      max_years_ago = current_year - blog_start_year
       years_ago_data = []
 
       # Iterate for 1 to 6 years ago
-      (1..6).each do |years_ago|
+      (1..max_years_ago).each do |years_ago|
         target_year = current_date.year - years_ago
         target_month = current_date.month
         target_day = current_date.day
-
+        
+        # Adjust for leap years
+        if target_month == 2 && target_day == 29 && !Date.leap?(target_year)
+          target_day = 28  # Adjust to February 28 for non-leap years
+        end
+        
         # Find a post with the same month and day in the target year
         matching_post = site.posts.docs.find do |p|
           p.data['date'].year == target_year &&

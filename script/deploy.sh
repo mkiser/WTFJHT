fcb3d7f4ec1fcf4ec29b28cb744a9f62ddf9363b
Matt <matt@whatthefuckjusthappenedtoday.com>
2025-03-24 22:30:48 -0700

    updates to this bs

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 set -e
 
-echo "🟢 Starting deploy to S3…"
-
-# Optional: echo current AWS identity
-aws sts get-caller-identity
-
-# Deploy to S3 (adjust region if needed)
-aws s3 sync _site s3://whatthefuckjusthappenedtoday.com
-
-echo "✅ Deploy complete"
+echo "Deploying _site to S3…"
+aws s3 sync _site s3://whatthefuckjusthappenedtoday.com --exact-timestamps

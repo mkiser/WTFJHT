8d0f3c7bb7a5ebd2dce64cb41887936139331eb3
Matt <matt@whatthefuckjusthappenedtoday.com>
2025-07-31 11:38:33 -0700

    fixing urls

--- service-worker.js
+++ service-worker.js
@@ -2,8 +2,8 @@ const CACHE_NAME = "wtfjht-cache-v1";
  const urlsToCache = [
   "/",
   "/manifest.json",
-  "{{ site.baseurl }}/uploads/wtfjht-logo192.png",
-  "{{ site.baseurl }}/uploads/wtfjht-logo512.png"
+  "/uploads/wtfjht-logo192.png",
+  "/uploads/wtfjht-logo512.png"
 ];
 
 self.addEventListener("install", event => {
